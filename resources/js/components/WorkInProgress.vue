3e9ccb825f2f431acd8f0383ed3cbac4a0efe759
tgiannotta <t.giannotta92@gmail.com>
2022-02-21 15:27:46 +0100

    end frontoffice

---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,12 @@
+<template>
+    <WorkInProgress />
+</template>
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue';
+export default {
+    name:'App',
+    components:{
+        WorkInProgress
+    }
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
@@ -26,3 +24,7 @@ Route::middleware('auth')
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('home');
     });
+
+Route::get('{any?}', function(){
+    return view('guests.home');
+})->where("any", ".*");
